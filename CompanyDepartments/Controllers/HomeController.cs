ae84e5afc38856e8e56debfe26e07c47464ef603
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-05-15 11:45:12 +0300

    Удалена страница Privacy

--- CompanyDepartments/Controllers/HomeController.cs
+++ CompanyDepartments/Controllers/HomeController.cs
@@ -13,11 +13,6 @@ namespace CompanyDepartments.Controllers
             _logger = logger;
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
